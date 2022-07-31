edc673f03120aea13f83b1b327dca50c83924c2d
miabe23-meet <109899471+miabe23-meet@users.noreply.github.com>
2022-07-31 14:32:37 +0300

    lab started

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -6,13 +6,44 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
+config= {
+  "apiKey": "AIzaSyA2L6-df-klrNcmF4DELH7pgO74OLN5MU4",
+  "authDomain": "fir-cfa5b.firebaseapp.com",
+  "projectId": "fir-cfa5b",
+  "storageBucket": "fir-cfa5b.appspot.com",
+  "messagingSenderId": "949101888347",
+  "appId": "1:949101888347:web:2cfb886b3e4e9e5250907e",
+  "measurementId": "G-Z6G20GVL9W"
+}
+
+firebase=pyrebase.initializes_app(config)
+auth=firebase.auth()
+
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    error = ""
+    if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('home'))
+       except:
+           error = "Authentication failed"
     return render_template("signin.html")
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    error = ""
+    if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('add_tweet'))
+       except:
+           error = "Authentication failed"
     return render_template("signup.html")
 
 
